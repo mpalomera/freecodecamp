4ac1b33adda3c2ca47680fd0f95e92206f1201f6
Ubuntu <ubuntu@ip-10-0-0-47.ec2.internal>
2021-11-22 22:10:31 +0000

    adding timestamp project

---
+++ timestamp/amplify/backend/function/timestamp8e3e2b2d/src/app.js
@@ -0,0 +1,70 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+const getDate = (newDate) => {
+  const unix = Math.round(newDate.getTime());
+  const utc = newDate.toString();
+  return {unix, utc};
+}
+
+function convertToInt(value) {
+  if (/^[-+]?(\d+|Infinity)$/.test(value)) {
+    return Number(value)
+  } else {
+    return NaN
+  }
+}
+
+app.get('/api', function(req, res) {
+  const today = new Date();
+  res.json(getDate(today));
+});
+
+app.get('/api/:date', function(req, res) {
+  const date = req.params.date;
+  let unix =  convertToInt(date);
+  let newDate;
+  if(!isNaN(unix)){
+    newDate = new Date(unix);
+  }else {
+    newDate = new Date(date);
+  }
+  if(newDate == 'Invalid Date'){
+    return res.json({error : 'Invalid Date'});
+  }
+  res.json(getDate(newDate));
+});
+
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
---
+++ timestamp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "timestamp8e3e2b2d": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "timestamp": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
